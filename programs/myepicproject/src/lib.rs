9e9d449c62306e0e67d560a9b2cfa8179ce13300
A <97649928+the-decentralizer@users.noreply.github.com>
2022-07-23 17:39:37 -0400

    First solana program, currently holds a counter with an increment function

---
+++ programs/myepicproject/src/lib.rs
@@ -0,0 +1,46 @@
+use anchor_lang::prelude::*;
+
+declare_id!("DP2bB5qqaQDyQWYXKy4ixE8wWXLzNh5F2kp4RJqRNkFf");
+
+#[program]
+pub mod myepicproject {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+    // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account;
+    // Initialize total_gifs.
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+  // increment gif counter
+  pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+    // grab ref to base account then increment counter
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs += 1;
+    Ok(())
+  }
+
+}
+
+// Attach certain variables to the StartStuffOff context.
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Data for addGif Context
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
---
+++ tests/myepicproject.js
@@ -0,0 +1,51 @@
+const anchor = require("@project-serum/anchor");
+
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
+
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  //create and set provider
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Myepicproject;
+
+  // Create a keypair for the account
+  const baseAccount = anchor.web3.Keypair.generate();
+  // Call start_stuff_off, pass it the params
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+  console.log("📝 Your transaction signature", tx);
+  // Fetch data from this account
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // call add_gif
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  // regrab account to see change
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 New GIF Count", account.totalGifs.toString());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
